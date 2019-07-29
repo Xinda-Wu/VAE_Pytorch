c93a6eee377d7bdde1d5f50142cc2b70c52c70f6
Xinda <xinda@XindadeiMac.local>
2019-07-29 13:10:59 +0800

    simple_autoencoder

---
+++ 01_simple_autoencoder.py
@@ -0,0 +1,120 @@
+'''
+吴鑫达：
+autoencoder：
+input：图片
+encoder
+decoder
+output：图片
+
+loss: MSELoss
+'''
+
+import os
+import torch
+from torch import nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.datasets import MNIST
+from torchvision.utils import save_image
+
+num_epoch = 100
+batch_size = 128
+learning_rate = 1e-3
+img_size = 784
+
+# 设备配置
+torch.cuda.set_device(0)  # 这句用来设置pytorch在哪块GPU上运行
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+if not os.path.exists('./img'):
+    os.mkdir("./img")
+
+
+def to_img(x):
+    out = 0.5*(x+1)
+    out = out.clamp(0, 1)
+    out = out.view(-1, 1, 28, 28)
+    return out
+
+
+img_transformer = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize([0.5], [0.5])
+])
+
+dataset = MNIST(
+    root='./data/MNIST',
+    train=True,
+    transform=img_transformer,
+    download=False
+)
+
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+# =======================Model=======================
+class autoencoder(nn.Module):
+    def __init__(self):
+        super(autoencoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(img_size, 128),
+            nn.ReLU(True),
+            nn.Linear(128, 64),
+            nn.ReLU(True),
+            nn.Linear(64, 12),
+            nn.ReLU(True),
+            nn.Linear(12, 3)
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(3, 12),
+            nn.ReLU(True),
+            nn.Linear(12, 64),
+            nn.ReLU(True),
+            nn.Linear(64, 128),
+            nn.ReLU(True),
+            nn.Linear(128, img_size),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+
+# ======================= Loss & Optim =======================
+model = autoencoder().to(device)
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-5)
+
+# ======================= Train =======================
+for epoch in range(num_epoch):
+    for data in dataloader:
+        img, _ = data
+        # 将图像数据从二维[28，28]展成一维向量[784], 每行代表一张图片，共128行；列数为784列
+        img = img.view(img.size(0), -1)
+
+        # Cuda train
+        img = Variable(img).to(device)
+        output = model(img)
+        loss = criterion(output, img)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    # ========== log ===========
+    print('epoch [{}/{}], loss:{:.4f}'.format(epoch+1, num_epoch, loss.item()))
+
+    # ========== save img =======
+    if epoch % 10 == 0:
+        pic = to_img(output.cpu().data)
+        save_image(pic, './img/img_{}.png'.format(epoch))
+
+torch.save(model.state_dict(), './sample_autoencoder.pth')
+
+
+
+
+
+
